2afd832309a48482a7ecd4f6cf8d111d4028e459
Alimov-8 <alimov.abdullokh8@gmail.com>
2021-03-20 12:41:17 +0500

    lab5-ex4 done

--- webpage/data.php
+++ webpage/data.php
@@ -6,52 +6,59 @@
 </head>
 
 <body>
-<h1> Thanks, sucker!</h1>
 
-<p>
-Your information has been recorded.
-</p>
+<?php if($_REQUEST["name"] != "" && isset($_REQUEST["section"]) && $_REQUEST["card"] != "" && $_REQUEST["card_type"] != "") { ?>
+    <h1> Thanks, sucker!</h1>
+    <p>
+    Your information has been recorded.
+    </p>
 
-<hr />
+    <hr />
 
-<dl>
-    <dt>Name</dt>
-    <dd>
-        <?=$_REQUEST["name"]?>
-    </dd>
+    <dl>
+        <dt>Name</dt>
+        <dd>
+            <?=$_REQUEST["name"]?>
+        </dd>
 
-    <dt>Section</dt>
-    <dd>
-        <?=$_REQUEST["section"]?>
-    </dd>
+        <dt>Section</dt>
+        <dd>
+            <?=$_REQUEST["section"]?>
+        </dd>
 
-    <dt>Credit Card</dt>
-    <dd>
-        <?=$_REQUEST["card"]?>
-        (<?=$_REQUEST["card_type"]?>)
-    </dd>
-</dl>
-<p>Here are all the suckers who have submitted here:</p>
+        <dt>Credit Card</dt>
+        <dd>
+            <?=$_REQUEST["card"]?>
+            (<?=$_REQUEST["card_type"]?>)
+        </dd>
+    </dl>
+    <p>Here are all the suckers who have submitted here:</p>
 
-<?php
-$file = 'suckers.txt';
-// Open the file to get existing content
-$current = file_get_contents($file);
-// Append a new person to the file
-$current .=  $_REQUEST["name"].";". $_REQUEST["section"].";".$_REQUEST["card"].";".$_REQUEST["card_type"].";"."\n";
-// Write the contents back to the file
-file_put_contents($file, $current);
-?>
-<pre>
     <?php
-    if ($file = fopen("suckers.txt", "r")) {
-    while(!feof($file)) {
-        $line = fgets($file);
-        print $line;
-    }
-    fclose($file);
-}
+    $file = 'suckers.txt';
+    // Open the file to get existing content
+    $current = file_get_contents($file);
+    // Append a new person to the file
+    $current .=  $_REQUEST["name"].";". $_REQUEST["section"].";".$_REQUEST["card"].";".$_REQUEST["card_type"].";"."\n";
+    // Write the contents back to the file
+    file_put_contents($file, $current);
     ?>
-</pre>
+    <pre>
+        <?php
+        if ($file = fopen("suckers.txt", "r")) {
+        while(!feof($file)) {
+            $line = fgets($file);
+            print $line;
+        }
+        fclose($file);
+    }
+        ?>
+    </pre>
+<?php }  else {?>
+    <h1> Sorry</h1>
+    <p>
+        You didn't fill out the form completely. <a href="index.php">Try Again?</a>
+    </p>
+<?php } ?>
 </body>
 </html>
